df5d1d98790f2444a2a8ccf5580968731b37c104
KRISHNA LATH <krishnalath99@gmail.com>
2024-05-14 11:00:22 +0530

    Implementing React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import RootLayout from './Pages/RootLayout';
+import HeroHome from './Components/Hero/HeroHome';
+import MainPage from './Components/MainPage/MainPage';
+import SignInForm from './Components/SignInForm/SignInForm';
 
-import Navbar from "./Components/Navbar/Navbar"
-import Hero from './Components/Hero/Hero'
-import Services from './Components/Services/Services'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <div className="container">
-        <Title subtitle='OUR SERVICES' title='What We Offer'/>
-        <Services />
-        <About />
-        <Title subtitle='Contact Us' title='Get in Touch'/>
-        <Contact />
-        <Footer />
-      </div>
-      
-    </div>
-  )
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+        children: [
+          {
+            index:true,
+            element: <HeroHome />
+          },
+          {
+            path:'/login',
+            element: <SignInForm />
+          }
+        ]
+      }
+
+    ]
+  }
+])
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App
--- src/Components/Hero/HeroHome.jsx
+++ src/Components/Hero/HeroHome.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import './Hero.css'
 import dark_arrow from '../../assets/dark_arrow.png'
+import { Link } from 'react-router-dom'
 
-const HeroHome = ({onButtonClick}) => {
+const HeroHome = () => {
   return (
       <div className="hero-text">
         <h1>Driving Confidence, Securing Your Every Mile.</h1>
         <p>Our commitment to excellence drives every aspect of our service, ensuring your satisfaction and peace of mind at Motor Shield.</p>
-        <button className='btn' onClick={onButtonClick}>Get Quotes<img src={dark_arrow} alt="" /></button>
+        <Link className='btn' to="/login">Get Quotes<img src={dark_arrow} alt="" /></Link>
       </div>
   )
 }
