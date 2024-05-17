2026294f896ab3bcbb1bd0012517e5adc06bfefd
KRISHNA LATH <krishnalath99@gmail.com>
2024-05-17 12:49:41 +0530

    Same Vehicle Registration not allowed

--- src/Components/PolicySubMenu/VehicleDetails.jsx
+++ src/Components/PolicySubMenu/VehicleDetails.jsx
@@ -13,6 +13,7 @@ const VehicleDetails = () => {
   const [registrationDate, setRegistrationDate] = useState('');
   const [seats, setSeats] = useState(1);
   const [purchasePrice, setPurchasePrice] = useState('');
+  const [er, setEr] = useState('')
 
   const navigate = useNavigate()
   
@@ -20,25 +21,32 @@ const VehicleDetails = () => {
     event.preventDefault();
     let token = localStorage.getItem("token")
     try {
-        const response = await axios.post('http://127.0.0.1:8000/add-vehicle/', {
-            vehicle_type:vehicleType,
-            vehicle_number:vehicleNumber,
-            make:make,
-            model:model,
-            fuel_type:fuelType,
-            registration_date:registrationDate,
-            seats:seats,
-            purchase_price:purchasePrice},
-          {headers:{
-            Authorization: token ? `Bearer ${token}` : null
-          }});
-        console.log('Vehicle Added:', response.data);
-        navigate('/addOns-details', { state: {vehicle_number: vehicleNumber}})
-      }
-      catch(error) {
-        console.log(error)
-      }
-    }  
+      const resp = await axios.get(`http://127.0.0.1:8000/vehicles/${vehicleNumber}/`, {
+        headers: {
+          Authorization: token? `Bearer ${token}` : null
+        }
+      });
+      setEr('This Vehicle Number is already Registered')
+      return
+    }
+    catch(error) {
+      console.log(error)
+      const response = await axios.post('http://127.0.0.1:8000/add-vehicle/', {
+        vehicle_type:vehicleType,
+        vehicle_number:vehicleNumber,
+        make:make,
+        model:model,
+        fuel_type:fuelType,
+        registration_date:registrationDate,
+        seats:seats,
+        purchase_price:purchasePrice},
+        {headers:{
+          Authorization: token ? `Bearer ${token}` : null
+      }});
+      console.log('Vehicle Added:', response.data);
+      navigate('/addOns-details', { state: {vehicle_number: vehicleNumber}})
+    }
+  }  
   return (
     <div className='new-form'>
       <form className="vehicle-form">
@@ -62,6 +70,7 @@ const VehicleDetails = () => {
         <input type="date" id="registrationDate" name="registrationDate" placeholder="Enter Vehicle's Registration Date" value={registrationDate} onChange={(event) => setRegistrationDate(event.target.value)} required />
         <input type="number" id="seats" name="seats" min="1" placeholder='Number of Seats' value={seats} onChange={(event) => setSeats(event.target.value)} required />
         <input type="number" id="purchasePrice" name="purchasePrice" placeholder="Enter Vehicle's Purchase Price" value={purchasePrice} onChange={(event) => setPurchasePrice(event.target.value)} required />
+        {er && <p className="add-error">{er}</p>}
         <Link to='hero' smooth={true} offset={0} duration={500} onClick={handleSubmit} className='btn light-btn'>Submit</Link>
       </form>
     </div>
