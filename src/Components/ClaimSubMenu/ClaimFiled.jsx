d438ae8680870417f83e653b10bc00c400a4d66e
KRISHNA LATH <krishnalath99@gmail.com>
2024-05-17 18:11:04 +0530

    Implementing Code Resuability 2

--- src/Components/ClaimSubMenu/ClaimFiled.jsx
+++ src/Components/ClaimSubMenu/ClaimFiled.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import '../ClaimSubMenu/ClaimFiled.css'
+import MessagePage from '../../Pages/MessagePage/MessagePage'
 
 const ClaimFiled = () => {
-
   return (
-    <div className='claim-filed-page'>
-        <h2>Claimed Filed</h2>
-        <br/>
-        <p>Your submission for the claim against the insurance policy has been received and duly processed.</p>
-        <br/> 
-        <p>"The claim details are presently undergoing thorough review for approval. Upon successful verification, we will process the claim."
-        </p>
-        <br/>
-        <p>Click here to track the claim</p>
-        <br/>
-        <Link to='/track-claim' className='btn light-btn'>Track Claim</Link>
-    </div>
+    <MessagePage 
+      heading={"Claimed Filed"}
+      firstLine={"Your submission for the claim against the insurance policy has been received and duly processed."}
+      secondLine={"The claim details are presently undergoing thorough review for approval. Upon successful verification, we will process the claim."}
+      thirdLine={"Click here to track the claim :"}
+      linkTo={'/track-claim'}
+      buttonName={'Track Claim'}/>
   )
 }
 
--- src/Components/PolicySubMenu/ProposalFiled.jsx
+++ src/Components/PolicySubMenu/ProposalFiled.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import './ProposalFiled.css'
+import MessagePage from '../../Pages/MessagePage/MessagePage';
 
 const ProposalFiled = () => {
-    return (
-        <div className='proposal-message-page'>
-          <div className="proposal-message">
-            <h2>Proposal Filed</h2>
-            <br/>
-              <p>Your proposal for the insurance policy has been duly submitted.</p>
-              <br/> 
-              <p>It is currently undergoing review for approval. Upon completion of background verification, you will be prompted to proceed with payment for the policy.
-            </p>
-            <br/>
-            <p>Click here to track the policy</p>
-            <br/>
-            <Link to='/track-policy' className='btn light-btn'>Track Policy</Link>
-          </div>
-        </div>
-      );
+  return (
+      <MessagePage 
+        heading={"Proposal Filed"}
+        firstLine={"Your proposal for the insurance policy has been duly submitted."}
+        secondLine={"It is currently undergoing review for approval. Upon completion of background verification, you will be prompted to proceed with payment for the policy."}
+        thirdLine={"Click here to track the policy"}
+        linkTo={'/track-policy'}
+        buttonName={"Track Policy"} />
+    );
 }
 
 export default ProposalFiled
